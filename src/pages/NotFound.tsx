01d733a1a2847912281982a88bb3a82c740ff169
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-06-23 00:03:10 +0300

    bye CNAME

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+// Define home React component
+import * as React from 'react';
+
+const NotFound = () => {
+    return (
+    <div>
+        <h1>Home</h1>
+    </div>
+    );
+}
+
+export default NotFound;
---
+++ src/pages/Others.tsx
@@ -0,0 +1,12 @@
+// Define home React component
+import * as React from 'react';
+
+const Others = () => {
+    return (
+    <div>
+        <h1>Others</h1>
+    </div>
+    );
+}
+
+export default Others;
