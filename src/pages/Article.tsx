81d013dfa97bf6801d4b57d1ff01bf26305b3c5c
Mititean Adorian <Mititean.Adorian@ro.bosch.com>
2022-07-01 15:32:12 +0300

    More css for paper and reactive

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import '../scss/Article.scss'
 import '../scss/Content.scss'
 
+import Paper from '../components/Paper'
+
 const Article = () => {
     
     const params = useParams()
@@ -9,6 +11,7 @@ const Article = () => {
     return (
         <div className="content-panel article">
             <h1>{params.title}</h1>
+            <Paper />
             <p> This is a pretty test. I really like this font. Is it for me? </p>
         </div>
     );
