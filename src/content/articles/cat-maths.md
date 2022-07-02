dec5bd603877be1c6826d01acfb97683c5c4aad3
Mititean Adorian <Mititean.Adorian@ro.bosch.com>
2022-07-02 10:55:39 +0300

    markdown article integration

---
+++ src/content/articles/cat-maths.md
@@ -0,0 +1,6 @@
+# This is one of the best articles ever
+
+## You would never believe what he said
+
+- [] Do you want this?
+- [] Or that?
--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,18 +1,44 @@
 import { useParams } from "react-router-dom";
 import '../scss/Article.scss'
 import '../scss/Content.scss'
-
-import Paper from '../components/Paper'
+import ReactMarkdown from 'react-markdown'
+import { useState, useEffect } from "react";
 
 const Article = () => {
     
+    const [content, setContent] = useState('');
     const params = useParams()
+
+    let dashedTitle = "article-not-found"
+    if(params.title) {
+        dashedTitle = params.title.replace(/ /g, '-')
+    }
+
+    useEffect(() => {
+        // Acts as a Promise to fetch the markdown file location
+        import(`../content/articles/${dashedTitle.toLowerCase()}.md`)
+            .then(res => {
+                // Fetch the data at the markdown location
+                fetch(res.default)
+                    .then(res => res.text())
+                    .then(res => setContent(res))
+                    .catch(
+                        err => {
+                            console.log(err)
+                        });
+            })
+            .catch(err => {
+                console.log(err)
+                setContent(`# Article not found`)
+            });
+    })
+
 
     return (
         <article className="content-panel">
-            <h1>{params.title}</h1>
-            {/* <Paper /> */}
-            <p> This is a pretty test. I really like this font. Is it for me? </p>
+            <ReactMarkdown>
+                {content}
+            </ReactMarkdown>
         </article>
     );
 }
