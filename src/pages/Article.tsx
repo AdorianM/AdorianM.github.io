769998ea1e296682357d2e395a5f7c54f3989e97
Mititean Adorian <Mititean.Adorian@ro.bosch.com>
2022-07-03 00:49:23 +0300

    Article doesn't render dynamically

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,57 +1,47 @@
 import { useParams } from "react-router-dom";
 import '../scss/Article.scss'
 import '../scss/Content.scss'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import remarkMath from 'remark-math'
-import rehypeKatex from 'rehype-katex'
 import { useState, useEffect } from "react";
 import 'doodle.css/doodle.css'
+
 import 'katex/dist/katex.min.css'
 
 import Test from '../content/articles/test.mdx'
+import Tech from '../content/articles/tech.mdx'
 
 const Article = () => {
     
-    const [content, setContent] = useState('');
     const params = useParams()
+    const [article, setArticle] = useState('')
 
     let dashedTitle = "article-not-found"
     if(params.title) {
         dashedTitle = params.title.replace(/ /g, '-')
     }
 
-    useEffect(() => {
-        // Acts as a Promise to fetch the markdown file location
-        import(`../content/articles/${dashedTitle.toLowerCase()}.md`)
-            .then(res => {
-                // Fetch the data at the markdown location
-                fetch(res.default)
-                    .then(res => res.text())
-                    .then(res => {
-                        console.log(res)
-                        setContent(res)
-                        })
-                    .catch(
-                        err => {
-                            console.log(err)
-                        });
-            })
-            .catch(err => {
-                console.log(err)
-                setContent(`# Article not found`)
-            });
-    })
+    // const file = remark().use(remarkMdx).processSync(Tech)
+
+    // useEffect(() => {
+    //     // Acts as a Promise to fetch the markdown file location
+    //     import(`../content/articles/${dashedTitle}.mdx`)
+    //         .then(res => {
+    //             // Fetch the data at the markdown location
+    //             setArticle(res);
+    //             console.log
+    //             return res
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         });
+    // })
 
 
     return (
         <article className="content-panel doodle">
-            <Test />
-            <ReactMarkdown remarkPlugins={[remarkGfm, remarkMath]}
-                           rehypePlugins={[rehypeKatex]}>
-                {content}
-            </ReactMarkdown>
-
+            <div className="article-content">
+                {/* import and render mdx with the dashedTitle */}
+                <Test />
+            </div>
         </article>
     );
 }
