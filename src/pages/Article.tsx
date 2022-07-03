f9d4752925b69d83bbed4cfb308aca257adc63e2
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-07-03 23:41:59 +0300

    article renders based on title

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -6,41 +6,31 @@ import 'doodle.css/doodle.css'
 
 import 'katex/dist/katex.min.css'
 
-import Test from '../content/articles/test.mdx'
-import Tech from '../content/articles/tech.mdx'
+import NotFound from '../content/articles/article-not-found.mdx'
 
 const Article = () => {
     
     const params = useParams()
-    const [article, setArticle] = useState('')
+    const [article, setArticle] = useState()
 
     let dashedTitle = "article-not-found"
     if(params.title) {
         dashedTitle = params.title.replace(/ /g, '-')
     }
 
-    // const file = remark().use(remarkMdx).processSync(Tech)
-
-    // useEffect(() => {
-    //     // Acts as a Promise to fetch the markdown file location
-    //     import(`../content/articles/${dashedTitle}.mdx`)
-    //         .then(res => {
-    //             // Fetch the data at the markdown location
-    //             setArticle(res);
-    //             console.log
-    //             return res
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         });
-    // })
-
+    useEffect(() => {
+        import(`../content/articles/${dashedTitle}.mdx`).then(module => {
+            setArticle(module.default)
+        }).catch(err => {
+            console.log(err)
+            setArticle(undefined)
+        })
+    }, [dashedTitle])
 
     return (
         <article className="content-panel doodle">
             <div className="article-content">
-                {/* import and render mdx with the dashedTitle */}
-                <Test />
+                {article ? article : <NotFound />}
             </div>
         </article>
     );
