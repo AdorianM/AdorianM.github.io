19079d752838b7b42f1bd45b1275542c007c93c0
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-07-05 22:08:39 +0300

    Simple text-image component

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -7,7 +7,6 @@ import 'doodle.css/doodle.css'
 import 'katex/dist/katex.min.css'
 
 import NotFound from '../content/articles/article-not-found.mdx'
-import SideTextImage from "../components/SideTextImage";
 
 const Article = () => {
     
@@ -31,11 +30,6 @@ const Article = () => {
     return (
         <article className="content-panel doodle">
             <div className="article-content">
-                <SideTextImage>
-                    <p>Test</p>
-                    <h1>Ha</h1>
-                    <img src="https://picsum.photos/200" alt=""/>
-                </SideTextImage>
                 {article ? article : <NotFound />}
             </div>
         </article>
