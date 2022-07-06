974588eec91a33634f289beba45e94ca40b08348
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-07-07 02:02:54 +0300

    Dynamically importing images

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../scss/GalleryItem.scss';
 
@@ -11,10 +11,31 @@ interface IGalleryItem {
 }
 
 const GalleryItem:FC<IGalleryItem> = (props: IGalleryItem) => {
+
+    const [image, setImage] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        console.log("image Path: ")
+        console.log(props.imagePath)
+        // If props.image is not an external url, then we need to load the image from the local file system
+        if(!props.imagePath.startsWith("http")) {
+            import(`../assets/images/${props.imagePath}.png`).then(module => {
+                setImage(module.default)
+            }).catch(err => {
+                console.log(err)
+                setImage(undefined)
+            })
+        } else {
+            setImage(props.imagePath)
+        }
+    }, [props.imagePath])
+
+    console.log(image)
+
     return (
         <Link className="gallery-item"
               to={`/${props.type}/${props.title.toLowerCase()}`}>
-            <img src={props.imagePath} alt={props.title} />
+            <img src={image} alt={props.title} />
             <span className={`gallery-item-info category-${props.type}`}>
                 <h1>{props.title}</h1>
                 <p>{props.description}</p>
