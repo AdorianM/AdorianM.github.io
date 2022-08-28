ee1facea7fd151ffcacaf0d377e2f9f304b9fb56
AdorianM <67052082+AdorianM@users.noreply.github.com>
2022-08-28 09:45:06 +0000

    Very poor working solution

--- pages/articles/[category]/[slug].tsx
+++ pages/articles/[category]/[slug].tsx
@@ -1,18 +1,43 @@
 import path from "path"
 import fs from 'fs'
 import React from "react"
+import yaml from 'js-yaml'
+import {unified} from 'unified'
+import {VFile} from 'vfile'
+import ReactMarkdown from 'react-markdown'
+import remarkParse from 'remark-parse'
+import remarkFrontmatter from 'remark-frontmatter'
+import remakrParseFrontmatter from 'remark-parse-frontmatter'
+
+const unifiedRemarkProcessor = unified().
+    use(remarkParse).
+    use(remarkFrontmatter).
+    use(remakrParseFrontmatter)
+
+interface FrontMatter {
+    title: string
+    date: string
+    category: string
+}
+
+const getFrontMatter = (markdown): FrontMatter => {
+    // const content = fs.readFileSync(markdown)
+    const mdFile = new VFile(markdown)
+    const data = unifiedRemarkProcessor.parse(mdFile)
+    if(data.children[0].type === 'yaml') {
+      return yaml.loadAll(data.children[0].value)[0] as FrontMatter
+    }
+    return {title: 'default', date: 'default', category: 'default'}
+}
 
 const Article = (props) => {
-
-    console.log("props: ")
-    console.log(props)
-
-    // The markdown is not rendered yet. It is just read. :(. We need to render it.
 
     return (
         <article className="content-panel doodle">
             <div className="article-content">
-                {props.markdown}
+                <ReactMarkdown remarkPlugins={[remarkFrontmatter]}>
+                  {props.markdown}
+                </ReactMarkdown>
             </div>
         </article>
     )
@@ -22,12 +47,16 @@ const postsDirectory = path.join(process.cwd(), 'content/posts')
 
 const getStaticPaths = async () => {
     const files = fs.readdirSync(postsDirectory)
-    const paths = files.map(filename => ({
-      params: {
-        category: "math",
-        slug: filename.replace('.mdx', '')
-      }
-    }))
+    const paths = files.map(file => {
+        const readFile = fs.readFileSync(path.join(postsDirectory, file)) // Really poor solution.
+        const frontMatter = getFrontMatter(readFile)
+        return {
+          params: {
+            category: frontMatter.category,
+            slug: file.replace('.mdx', '')
+          }
+        }
+    })
   
     return {
       paths,
